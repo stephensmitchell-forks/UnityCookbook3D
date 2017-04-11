2c8ddffa9bbda633ec0e7101b0012fe9dfec66ee
swingninja <josemulia@gmail.com>
2017-04-11 11:32:35 -0700

    MouseTouchShootBullets

---
+++ Assets/MouseTouchShootBullets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		// GetMouseButton - fires when left mouse click is pressed
+		// GetMouseButtonDown - fires a bullet everytime you do a left click
+		if (Input.GetMouseButton(0))
+		{
+			Fire();
+		}
+
+	}
+		
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 1.0f);        
+	}
+
+	void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	void OnMouseDrag(){
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = cursorPosition;
+	}
+}
